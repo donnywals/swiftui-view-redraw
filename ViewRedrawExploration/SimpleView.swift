45d189b0a42dd08ce5424df816dc17bee7506958
Donny Wals <donnywals@gmail.com>
2021-11-07 18:23:25 +0100

    Simple example updates

--- ViewRedrawExploration/SimpleView.swift
+++ ViewRedrawExploration/SimpleView.swift
@@ -5,4 +5,38 @@
 //  Created by Donny Wals on 07/11/2021.
 //
 
-import Foundation
+import SwiftUI
+
+struct SimpleListView: View {
+    @StateObject var listItems = SimpleListItems()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                    if case let .loaded(items) = listItems.state {
+                        ForEach(items) { item in
+                            SimpleCellView(item: item)
+                        }
+                    } else {
+                        ProgressView()
+                    }
+                }
+            }
+            .navigationTitle(Text("Simple example"))
+        }
+    }
+}
+
+struct SimpleCellView: View {
+    let item: SimpleItem
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(item.computed)
+                Spacer()
+            }
+        }.padding()
+    }
+}
